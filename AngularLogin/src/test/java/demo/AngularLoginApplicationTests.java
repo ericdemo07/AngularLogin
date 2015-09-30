eb1f2fa75a597fc9798de99699038eb174798993
Ayush Shukla <ericdemo07@gmail.com>
2015-09-30 17:50:41 +0530

    Changes: Maven -> Gradle  , Spring Boot added

---
+++ AngularLogin/src/test/java/demo/AngularLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AngularLoginApplication.class)
+public class AngularLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
